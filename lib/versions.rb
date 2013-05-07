ebd1338c3af253d579ab9fd578a0e3fd0ee72407
Mike Harris <mharris717@gmail.com>
2013-05-07 14:27:24 -0400

    test

--- lib/versions.rb
+++ lib/versions.rb
@@ -1,2 +1,17 @@
+class File
+  class << self
+    def create(filename,str)
+      open(filename,"w") do |f|
+        f << str
+      end
+    end
+    def append(filename,str)
+      open(filename,"a") do |f|
+        f << str
+      end
+    end
+  end
+end
+
 require 'website_versions'
 WebsiteVersions.tags = %w(v0.9.1 v0.9.5 v1.0.0-pre.4 v1.0.0-pre.2 v1.0.0-rc.2)
